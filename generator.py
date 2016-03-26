9774d68105926e5eabfddad3e8c976338179bd0a
David B. Adrian <dawidh.adrian@googlemail.com>
2016-03-26 17:03:21 +0100

    before making it worse

---
+++ generator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+
+import ujson
+
+from jinja2 import Environment, FileSystemLoader
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'template')),
+    trim_blocks=False)
+
+
+def render_template(template_filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+
+
+def create_network_py(network, out):
+    context = {
+        "meta": network["meta"],
+        "data": network["data"],
+        "network": network["network"],
+        "loss": network["loss"],
+    }
+    #
+    with open(out, 'w') as f:
+        py = render_template('network.tpl', context)
+        f.write(py)
+
+
+########################################
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--network', '-n', type=str, default="",
+                        help='The JSON definition of the network')
+    parser.add_argument('--out', '-o', type=str, default="",
+                        help='The generated network')
+    args = parser.parse_args()
+    # Load Json
+    with open(args.network, 'r') as f:
+        nn = ujson.load(f)
+
+    # Generate Network for the lolz
+    create_network_py(nn, args.out)
